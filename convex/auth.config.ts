9a07cfc3075b9566458b4831ad190835e7a0da73
@IMatvicevsky <matvicevsky_ilya@mail.ru>
2024-12-10 15:23:17 +0300

    add main flow

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+	providers: [
+		{
+			domain: 'https://eternal-airedale-8.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+}
---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+	{
+		id: 'blank',
+		label: 'Blank Document',
+		imageUrl: '/templates/blank-document.svg',
+	},
+	{
+		id: 'business_letter',
+		label: 'Business Letter',
+		imageUrl: '/templates/business-letter.svg',
+	},
+	{
+		id: 'cover_letter',
+		label: 'Cover Letter',
+		imageUrl: '/templates/cover-letter.svg',
+	},
+	{
+		id: 'letter',
+		label: 'Letter',
+		imageUrl: '/templates/letter.svg',
+	},
+	{
+		id: 'project_proposal',
+		label: 'Project Proposal',
+		imageUrl: '/templates/project-proposal.svg',
+	},
+	{
+		id: 'resume',
+		label: 'Resume',
+		imageUrl: '/templates/resume.svg',
+	},
+	{
+		id: 'software_proposal',
+		label: 'Software Proposal',
+		imageUrl: '/templates/software-proposal.svg',
+	},
+]
